3f78965c9e908bba9033bab9cf83a90f3569cf03
Jaden <boogers32107@gmail.com>
2025-02-11 14:14:30 +0000

    Changes 1

---
+++ Haiku.py
@@ -0,0 +1,19 @@
+import time
+import requests
+import random
+
+def generate_haiku():
+    syllables = [5, 7, 5]
+    haiku = []
+    for s in syllables:
+        line = ' '.join(random.choice() for _ in range(s)) #We need a API something
+        haiku.append(line)
+    return '\n'.join(haiku)
+
+def post_to_discord(haiku):
+    webhook_url = "Teacher will provide webhook"
+    payload = {
+        "content": haiku
+    }
+    response = requests.post(webhook_url, json=payload)
+    print(response.status_code)
