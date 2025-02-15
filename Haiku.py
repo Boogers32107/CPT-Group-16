7523eef82d24789e23ebeb58f949a423b6451af8
Jaden <boogers32107@gmail.com>
2025-02-15 15:17:17 +0000

    Just need list

--- Haiku.py
+++ Haiku.py
@@ -1,7 +1,7 @@
 import requests
 import random
 
-headline_list = ['Firstword','random', 'uber', 'snake', 'pear', 'frog', 'apple' ] # Need list of headlines or something Problably from X/Twitter
+headline_list = ['Testing','Work', 'Car', 'House', 'Food', 'Animal', 'Fruit' ] # Need list of headlines or something Problably from X/Twitter
 
 def generate_haiku():
     syllables = [5, 7, 5]
@@ -12,8 +12,8 @@ def generate_haiku():
     return '\n'.join(haiku)
 
 def post_to_discord(haiku):
-   webhook_url = 'https://discord.com/channels/1336355522215153688/1336355522818998397' # I had to add a URL that works for it to not error out just used one for a Server I just created
-   payload = { 'content': haiku }
+   webhook_url = 'https://discord.com/api/webhooks/1339685758273061016/COYZDNyTFckb1PX2PDalu-LUnaea-BEIeQsG9c4ca1my9x2dZTmITiFinjRkjN0Dhini' # I had to add a URL that works for it to not error out just used one for a Server I just created
+   payload = { "content": f"Generated Haiku Group 16:\n{haiku}" }
    response = requests.post(webhook_url, json=payload)
    print(response.status_code)
    print("Haiku posted to Discord:")
@@ -23,4 +23,3 @@ def post_to_discord(haiku):
 if __name__ == "__main__":
     haiku = generate_haiku()
     post_to_discord(haiku)
-    
